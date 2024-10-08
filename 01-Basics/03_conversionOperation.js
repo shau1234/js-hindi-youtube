8c924be0e1926cdbc09e39028d3c12a6a6e3a56d
shau1234 <91527935+shau1234@users.noreply.github.com>
2024-10-08 14:26:42 +0000

    Operators

--- 01-Basics/03_conversionOperation.js
+++ 01-Basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // here value of score is 33abc which is not a number but we are trying to convert that value to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // here value of score is 33abc which is not a number but we are trying to convert that value to number
 //iska type Number ho jayega but iski value NaN aayegi becoz we are trying to convert a string into number
 //let score = "33abc" o/p NaN(Not a Number)
 //let score = null o/p 0
@@ -14,7 +14,7 @@ console.log(valueInNumber); // here value of score is 33abc which is not a numbe
 
 let isLoggedIn = "shaistha"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); //o/p true 
+//console.log(booleanIsLoggedIn); //o/p true 
 /*
 1 => true 0=> false
 "" => false
@@ -23,6 +23,41 @@ console.log(booleanIsLoggedIn); //o/p true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// =========== Operations ===============
+
+let value = 3
+let negVal = -value
+// console.log(negVal);
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2); // 2 to the power 2 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " shaistha"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // o/p 122
+// console.log(1 + 2 + "2"); // o/p 32
+// console.log(1 + "2" + 2); // o/p 122
+
+console.log(+true); // o/p 1
+console.log(+""); // o/p 0
+console.log(true + true); // o/p 2
+
+let getCounter = 100
+getCounter++
+++getCounter
+console.log(getCounter);
+
+//link to study 
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
