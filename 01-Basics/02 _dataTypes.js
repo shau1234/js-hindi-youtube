c42ede0ffe0ff08fdb147ce4482549ac65179031
shau1234 <91527935+shau1234@users.noreply.github.com>
2024-10-08 08:33:47 +0000

    Datatypes

---
+++ 01-Basics/02 _dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3 + 3) // we are using node.js, not browser so can't use alert 
+
+console.log(3 + 
+    3) //code readability should be high avoid writting like this
+console.log("Shaistha")
+
+let name = "Shaistha"
+let age = 24
+let isLoggedIn = false
+
+/* Primitive DataTypes 
+number -> 2 to power 53
+BigInt if range is more than number
+String use ""
+Boolean => true/false
+null it is a standalone value hai specially type hai ek
+undefined -> let state; matlab value assign nahi hai lekin null we can assign eg: let state = null
+symbol -> for uniqueness of components
+*/
+
+/* Objects
+
+*/
+console.log(typeof name );
+console.log(typeof null); // this prints Object as o/p becoz ye ek value hai 
+console.log(typeof undefined); // typeOf undefined is undefined only becoz ye ek type hai JS ka 
+
+
+
