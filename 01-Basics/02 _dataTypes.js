5286b885289809d509b8b8b10c5ae47367cdbdfe
shau1234 <91527935+shau1234@users.noreply.github.com>
2024-10-21 13:40:39 +0000

    stack and heap memory

--- 01-Basics/02 _dataTypes.js
+++ 01-Basics/02 _dataTypes.js
@@ -4,7 +4,7 @@
 
 console.log(3 + 
     3) //code readability should be high avoid writting like this
-console.log("Shaistha")
+//console.log("Shaistha")
 
 let name = "Shaistha"
 let age = 24
@@ -12,7 +12,7 @@ let isLoggedIn = false
 
 const id = Symbol ('123')
 const anotherId = Symbol ('123')
-console.log(id === anotherId);  //o/p: false becoz both values are refering to diff objects
+//console.log(id === anotherId);  //o/p: false becoz both values are refering to diff objects
 
 
 /* Primitive DataTypes (call by value) jab bhi aap inko kahin se kahin copy karte hai jo inka org data hai wo reference memory ka nahi diya jata 
@@ -47,6 +47,42 @@ let myobj = {
 }
 
 const myFunction = function(){ // type of this myFunction is a object function 
-    console.log("Hello World");
+    //console.log("Hello World");
     
 }
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ======== stack & heap memory ==========
+// stack (primitive) heap (Non-Primitive)
+//whatever the value we get from stack is always a copy not the org but in heap we always get a reference of the value 
+
+let myCourseName = "JavaScript"
+
+let myOtherCourseName = myCourseName  // here myOtherCourseName will get a copy of myCourseName
+myOtherCourseName = "Java8" // here that copy is changed to Java8 
+console.log(myCourseName); // o/p JavaScript
+console.log(myOtherCourseName); // o/p Java8
+
+let userOne = {
+    emaiId : "user@google.com",
+    upiId: "user@ybl"
+}
+
+let userTwo = userOne 
+/*
+here reference of userOne is given i.e
+{
+    emaiId : "user@google.com",
+    upiId: "user@ybl"
+} this is in userTwo now 
+*/
+userTwo.emaiId="shaistha@google.com" // emailId is changed and it will be updated for userOne as well
+//becoz both the variables are referring to same object so what ever the changes we do, we do to the org value not copy
+console.log(userOne.emaiId); //o/p shaistha@google.com
+console.log(userTwo.emaiId);// o/p shaistha@google.com
+
+
+
+
