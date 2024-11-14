6a245954b85f1d75746a8cc5ac6e5d57423fc0ac
shau1234 <91527935+shau1234@users.noreply.github.com>
2024-11-14 08:13:57 +0000

    Arrays slice ans splice

--- 02-Basics/01_Arrays.js
+++ 02-Basics/01_Arrays.js
@@ -7,14 +7,39 @@
 const myArr = [0, 1, 3, 7, 6, 8]
 const myHeros = ["Caps", "Ironman"]
 const myArr2 = new Array(1,2,3,4,6)
-console.log(myArr2[2]); // o/p 3 
+//console.log(myArr2[2]); // o/p 3 
 
 //Array methods
 
 myArr.push(9)
 myArr.pop() //Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
 
-myArr.unshift(2) // o/p: [2, 0, 1, 3, 7, 6, 8] //Inserts new elements at the start of an array, and returns the new length of the array.
+//myArr.unshift(2) // o/p: [2, 0, 1, 3, 7, 6, 8] //Inserts new elements at the start of an array, and returns the new length of the array.
+myArr.shift() // o/p: [ 0, 1, 3, 7, 6, 8 ] // Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
 
-console.log(myArr);
+
+// console.log(myArr.includes(9)); //Determines whether an array includes a certain element, returning true or false as appropriate.
+// console.log(myArr.indexOf(88));// o/p:-1 Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+
+const newArr = myArr.join() // Adds all the elements of an array into a string, separated by the specified separator string.
+
+// console.log(myArr); //o/p : [ 1, 3, 7, 6, 8 ]
+// console.log(newArr); // o/p : 1,3,7,6,8
+
+
+// slice, splice
+
+console.log("A: "+myArr ); // o/p A: 1,3,7,6,8
+
+const myn1 = myArr.slice(1,3) //Returns a copy of a section of an array. For both start and end, a negative index can be used to indicate an offset from the end of the array. For example, -2 refers to the second to last element of the array.
+console.log(myn1); // o/p : [ 3, 7 ]
+console.log("B: "+myArr); // o/p B: 1,3,7,6,8
+
+
+const myn2 = myArr.splice(1,3) //Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+console.log(myn2); //o/p: [ 3, 7, 6 ]
+console.log("C: "+myArr); //o/p C: 1,8
+
+
+
 
